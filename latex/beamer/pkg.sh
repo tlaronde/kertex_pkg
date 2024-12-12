f3031c9e8870e65fa08f8aa5d8995c1eba88878c
Thierry LARONDE <tlaronde@kergis.com>
2024-12-12 19:32:43 +0100

    latex/beamer: 2 dependencies missing; addd art/ subdir to search
    tex/stringenc and latex/extsizes are also dependencies. Added.
    
    Examples can try to reach data in the art/ subdir: ad it to KXPATH.
    
    Display a message about generation of an example.

--- latex/beamer/pkg.sh
+++ latex/beamer/pkg.sh
@@ -22,7 +22,16 @@ pkg_post_build()
 
 pkg_post_apply()
 {
-	return 0 # bash errors on empty function...
+	pkg_log 
+	pkg_log "You can generate a sample by processing:"
+	pkg_log "doc/examples/a-conference-talk/beamerexample-conference-talk.tex"
+	pkg_log
+	pkg_log "(the other example is in german so needs this lang"
+	pkg_log "  compiled in in latex)"
+	pkg_log
+	pkg_log "Do run twice latex on it, since the summary and"
+	pkg_log "  cross-references need a second pass."
+	pkg_log
 }
 
 pkg_post_remove()
@@ -91,6 +100,8 @@ DESCRIPTION:
 DEPENDENCIES:
 	fonts/sansmathaccent
 	tex/pgf
+	tex/stringenc
+	latex/extsizes
 	latex/geometry
 	latex/hyperref
 	latex/etoolbox
@@ -98,7 +109,8 @@ DEPENDENCIES:
 	latex/xcolor
 KERTEX_VERSION: 0.99.23.0
 KXPATH:
-	latex beamer;beamer/emulation;beamer/patch;beamer/multimedia;
+	latex beamer;beamer/art;beamer/emulation;
+	latex beamer/multimedia;beamer/patch;
 	latex beamer/themes/outer;beamer/themes/font;
 	latex beamer/themes/color;beamer/themes/inner;
 	latex beamer/themes/theme;beamer/themes/theme/compatibility;
